b0a6dfae13cdede24e74025916bde905d1fcb7fe
patricio <patoare@hotmail.com>
2025-01-21 22:18:19 +0100

    last of changes tuesday

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -6,7 +6,25 @@ export const AuthContext = createContext()
 const AuthContextProvider = ({children}) => {
     const [token, setToken] = useState()
     const [isAuthenticated, setIsAuthenticated] = useState(false)
-
+const verifyToken = async(tokenToVerify) => {
+    try {
+const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/verify`, {
+    headers: {
+        Authorization: `Bearer ${tokenToVerify}`
+    }
+})
+if(response.ok) {
+    const data = await response.json()
+    console.log(data)
+    setToken(tokenToVerify)
+} else {
+    localStorage.removeItem('authToken')
+}
+    } catch(error){
+console.log(error)
+localStorage.removeItem('authToken')
+    }
+}
     useEffect (() => {
         if(token) {
             setIsAuthenticated(true)
@@ -17,7 +35,7 @@ const AuthContextProvider = ({children}) => {
     useEffect(() => {
         const storageToken = localStorage.getItem('authToken')
         if(storageToken) {
-            setToken(storageToken)
+            verifyToken(storageToken)
         }
     })
 
